c13688792852f220e6f0cb5ecc4091b9a444cf37
Your Name <you@example.com>
2022-10-22 15:11:53 -0600

    finished client post

--- app.py
+++ app.py
@@ -1,69 +1,70 @@
-from unittest import result
 from apihelper import check_endpoint_info
 import dbhelper
 from flask import Flask, request, make_response
 import json
 import dbcreds
 from uuid import uuid4
+import client.client
 
 
 app = Flask(__name__)
 
+
+
 @app.get('/api/client')
 def client_info():
-    invalid = check_endpoint_info(request.args, ['client_id'])
+    return client.client.get()
+
+
+
+@app.post('/api/client')
+def client_post():
+    return client.client.post
+
+
+
+
+#---------------------------------------------------------------------------------#
+#client login
+
+
+@app.post('/api/client_login')
+def client_login():
+    invalid = check_endpoint_info(request.json, ['email','password'])
     if(invalid != None):
+        return make_response(json.dumps(invalid, default=str), 400)    
+
+    token = uuid4().hex
+    results = ('CALL client_login(?,?,?)',
+    [request.json.get('email'), request.json.get('password'), token])
+    
+    if(type(results) == list and results[0][0] == 1):
+        return make_response(json.dumps(results, default=str), 200)
+    elif(type(results) == list and results[0][0] == 0):
+        return make_response(json.dumps("Bad login attempt"), 400)
+    else:
+        return make_response(json.dumps("Sorry there has been an error"), 500) 
+    
+
+#---------------------------------------------------------------------------------#
+#restaurant specific info
+
+@app.get('/api/restaurant')
+def get_specific_restaurant():
+    invalid = check_endpoint_info(request.args, ['restaurant_id'])
+    if(invalid == None):
         return make_response(json.dumps(invalid, default=str), 400)
 
-
-    results = dbhelper.run_statment('CALL get_client(?)',
-    [request.args.get('client_id')])
+    results = dbhelper.run_statment('CALL', [request.args.get('restaurant_id')])
     if(type(results) == list):
         return make_response(json.dumps(results, default=str), 200)
     else:
         return make_response(json.dumps(results, default=str), 500)
 
 
-#---------------------------------------------------------------------------------#
-
-@app.post('/api/client')
-def client_post():
-    invalid = check_endpoint_info(request.json, ['client_email', 'client_fn', 'client_ln',
-     'client_img', 'client_username', 'client_password'])
-    if(invalid != None):
-        return make_response(json.dumps(invalid, default=str), 400)
-
-    token = uuid4().hex
-    results = dbhelper.run_statment('CALL post_client(?,?,?,?,?,?)', [request.json.get('client_email'), request.json.get('client_fn'), request.json.get('client_ln'),
-    request.json.get('client_img'), request.json.get('client_username'), request.json.get('client_password')])
-    if(type(results) == list):
-        return make_response(json.dumps(token, default=str), 200)
-    else:
-        return make_response(json.dumps(results, default=str), 500)
-
-
 
 #---------------------------------------------------------------------------------#
-
-
-
-@app.post('/api/client_login')
-def client_login():
-    invalid = check_endpoint_info(request.json, ['client_email','client_password'])
-    if(invalid != None):
-        return make_response(json.dumps(invalid, default=str), 400)    
-
-    token = uuid4().hex
-    results = ('CALL client_login(?,?,?)',
-    [request.json.get('client_email'), request.json.get('client_password'), token])
-    
-    if(type(results) == list and results[0][0] == 1):
-        return make_response(json.dumps(token, default=str), 200)
-    elif(type(results) == list and results[0][0] == 0):
-        return make_response(json.dumps("Bad login attempt"), 400)
-    else:
-        return make_response(json.dumps("Sorry there has been an error"), 500) 
-    
+#restaurant signup
 
 
 if(dbcreds.production_mode == True):
---
+++ client/client.py
@@ -0,0 +1,40 @@
+from apihelper import check_endpoint_info
+import dbhelper
+from flask import Flask, request, make_response
+import json
+import dbcreds
+from uuid import uuid4
+
+#---------------------------------------------------------------------------------#
+#client info
+
+def get():
+    invalid = check_endpoint_info(request.args, ['client_id'])
+    if(invalid != None):
+        return make_response(json.dumps(invalid, default=str), 400)
+
+
+    results = dbhelper.run_statment('CALL get_client(?)',
+    [request.args.get('client_id')])
+    if(type(results) == list):
+        return make_response(json.dumps(results, default=str), 200)
+    else:
+        return make_response(json.dumps(results, default=str), 500)
+
+
+#---------------------------------------------------------------------------------#
+#client signup
+
+def post():
+    invalid = check_endpoint_info(request.json, ['email', 'first_name', 'last_name',
+     'image_url', 'username', 'password'])
+    if(invalid != None):
+        return make_response(json.dumps(invalid, default=str), 400)
+
+    token = uuid4().hex
+    results = dbhelper.run_statment('CALL post_client(?,?,?,?,?,?,?)', [request.json.get('email'), request.json.get('first_name'), request.json.get('last_name'),
+    request.json.get('image_url'), request.json.get('username'), request.json.get('password'), token])
+    if(type(results) == list):
+        return make_response(json.dumps(results, default=str), 200)
+    else:
+        return make_response(json.dumps(results, default=str), 500)
